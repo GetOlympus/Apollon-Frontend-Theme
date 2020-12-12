61d6767f10531f3dfca0a4a8d75cbecdd86d9365
Achraf Chouk <achrafchouk@gmail.com>
2020-12-12 22:06:27 +0100

    typo

--- views/parts/element/author.php
+++ views/parts/element/author.php
@@ -12,7 +12,7 @@ if (!isset($_element)) {
     die('You are not authorized to directly access to this page');
 }
 
-echo  empty($_element['data']['author']) ? '' : sprintf(
+echo empty($_element['data']['author']) ? '' : sprintf(
     '%s %s',
     sprintf(
         '<i uk-icon="user" aria-hidden="%s"></i>',
