8b17a51bd955662cc8ff6e6ed93acfd4986e75ec
Achraf Chouk <achrafchouk@gmail.com>
2020-12-12 22:07:22 +0100

    typo

--- views/parts/block/thumbnail.php
+++ views/parts/block/thumbnail.php
@@ -12,23 +12,25 @@ if (!isset($_block)) {
     die('You are not authorized to directly access to this page');
 }
 
-if (!empty($_block['data']['images'])) {
-    echo sprintf(
-        '<figure class="%s"><div class="%s">',
-        'uk-grid-margin uk-container uk-container-xsmall uk-text-center',
-        'uk-inline-clip'.(empty($_block['css']) ? '' : ' '.$_block['css'])
-    );
-
-    if ($_block['canvas']) {
-        echo sprintf(
-            '<canvas width="%s" height="%d"></canvas>',
-            '100%',
-            $_block['data']['images']['height']
-        );
-    }
-
-    $rep = $_block['canvas'] ? 'uk-cover uk-img ' : '';
-    echo str_replace('<img ', '<img '.$rep, $_block['data']['images']['thumbnail']);
-
-    echo '</div></figure>';
+if (empty($_block['data']['images'])) {
+    return;
 }
+
+echo sprintf(
+    '<figure class="%s"><div class="%s">',
+    'uk-grid-margin uk-container uk-container-xsmall uk-text-center',
+    'uk-inline-clip'.(empty($_block['css']) ? '' : ' '.$_block['css'])
+);
+
+if ($_block['canvas']) {
+    echo sprintf(
+        '<canvas width="%s" height="%d"></canvas>',
+        '100%',
+        $_block['data']['images']['height']
+    );
+}
+
+$rep = $_block['canvas'] ? 'uk-cover uk-img ' : '';
+echo str_replace('<img ', '<img '.$rep, $_block['data']['images']['thumbnail']);
+
+echo '</div></figure>';
